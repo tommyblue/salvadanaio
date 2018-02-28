95b9d9b384d206965a8d69b13ee6221f06fb821e
Tommaso Visconti <tommaso.visconti@gmail.com>
2018-02-28 15:03:19 +0100

    [frontend] show movements categories

---
+++ frontend/src/components/Tag.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default ({title}) => (<span className="tag is-light">{title}</span>);
--- frontend/src/containers/Movements.js
+++ frontend/src/containers/Movements.js
@@ -7,6 +7,7 @@ import {formatMoney, formatISODate} from '../utils';
 import AccountSelector from '../components/AccountSelector';
 import DateRangeSelector from '../components/DateRangeSelector';
 import LoadingSpinner from '../components/LoadingSpinner';
+import Tag from '../components/Tag';
 
 const mapStateToProps = state => {
     return {
@@ -72,6 +73,7 @@ class Movements extends React.Component {
                 <thead>
                     <tr>
                         <th>Description</th>
+                        <th>Category</th>
                         <th>Amount</th>
                         <th>Operation date</th>
                         <th>Value date</th>
@@ -81,11 +83,9 @@ class Movements extends React.Component {
                 {_.map(this.props.movements, (m) => (
                     <tr key={`account_${m.id}`}>
                         <td>
-                            {m.short_description}
-                            <span className="movement-description tooltip is-tooltip-multiline" data-tooltip={m.description}>
-                                <i className="fas fa-info-circle" />
-                            </span>
+                            {this.showDescription(m)}
                         </td>
+                        <td>{this.showCategory(m.category)}</td>
                         <td>{formatMoney(m.amount)}</td>
                         <td>{formatISODate(m.operation_date)}</td>
                         <td>{formatISODate(m.value_date)}</td>
@@ -94,6 +94,29 @@ class Movements extends React.Component {
                 </tbody>
             </table>
         );
+    }
+
+    showDescription(movement) {
+        const description = movement.description ? (
+            <span
+                className="movement-description tooltip is-tooltip-multiline"
+                data-tooltip={movement.description}
+            >
+                <i className="fas fa-info-circle" />
+            </span>) : "";
+        return (
+            <span>
+                {movement.short_description}
+                {description}
+            </span>
+        );
+    }
+
+    showCategory(category) {
+        if (category) {
+            return (<Tag title={category.title} />);
+        }
+        return "";
     }
 }
 
