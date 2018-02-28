362e30b8d5bd034695e57cdef0ce33a57c380a48
Tommaso Visconti <tommaso.visconti@gmail.com>
2018-02-28 23:33:18 +0100

    fix chart data sorting

--- backend/lib/salvadanaio_web/controllers/api/v1/analytics/movements_controller.ex
+++ backend/lib/salvadanaio_web/controllers/api/v1/analytics/movements_controller.ex
@@ -6,7 +6,7 @@ defmodule SalvadanaioWeb.Api.V1.Analytics.MovementsController do
       SELECT amount, to_char(value_date, 'YYYYMM') AS d FROM movements
        WHERE value_date > CURRENT_DATE - interval '12 months') m
     GROUP BY d
-    ORDER BY d DESC;"
+    ORDER BY d ASC;"
     result = Ecto.Adapters.SQL.query!(Salvadanaio.Repo, query)
     render conn, "index.json", movements: result.rows
   end
