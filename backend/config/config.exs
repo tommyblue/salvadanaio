7fa740e89fc88146b75a418adf34c8abc03b230d
Tommaso Visconti <tommaso.visconti@gmail.com>
2018-03-12 14:41:04 +0100

    [backend] permit DELETE with CORS

--- backend/config/config.exs
+++ backend/config/config.exs
@@ -35,7 +35,7 @@ config :money,
 config :cors_plug,
   origin: ["*"],
   max_age: 86400,
-  methods: ["GET", "POST"]
+  methods: ["GET", "POST", "DELETE"]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
