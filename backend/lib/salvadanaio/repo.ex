fa26db7fc3479e2ff7728ab675902410b849d386
Tommaso Visconti <tommaso.visconti@gmail.com>
2018-03-15 15:32:01 +0100

    [backend] support both docker and local env

--- backend/lib/salvadanaio/repo.ex
+++ backend/lib/salvadanaio/repo.ex
@@ -3,16 +3,21 @@ defmodule Salvadanaio.Repo do
 
   @doc """
   Dynamically loads the repository url from the
-  DATABASE_URL environment variable.
+  DATABASE_URL environment variable or from specific
+  db env confs.
   """
   def init(_, config) do
-    config = config
-      |> Keyword.put(:username, System.get_env("PGUSER"))
-      |> Keyword.put(:password, System.get_env("PGPASSWORD"))
-      |> Keyword.put(:database, System.get_env("PGDATABASE"))
-      |> Keyword.put(:hostname, System.get_env("PGHOST"))
-      |> Keyword.put(:port, System.get_env("PGPORT") |> String.to_integer)
-    # {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
-    {:ok, config}
+    case {System.get_env("PGDATABASE"), System.get_env("PGHOST")} do
+      {nil, nil} ->
+        {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
+      _ ->
+        config = config
+          |> Keyword.put(:username, System.get_env("PGUSER"))
+          |> Keyword.put(:password, System.get_env("PGPASSWORD"))
+          |> Keyword.put(:database, System.get_env("PGDATABASE"))
+          |> Keyword.put(:hostname, System.get_env("PGHOST"))
+          |> Keyword.put(:port, System.get_env("PGPORT") |> String.to_integer)
+        {:ok, config}
+    end
   end
 end
