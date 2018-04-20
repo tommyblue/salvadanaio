7e3d17aa9c2e1f7450912450533f7755f45ea0e1
Tommaso Visconti <tommaso.visconti@gmail.com>
2018-04-20 12:49:19 +0200

    [backend] data for balance chart

--- backend/lib/salvadanaio_web/controllers/api/v1/analytics/balance_controller.ex
+++ backend/lib/salvadanaio_web/controllers/api/v1/analytics/balance_controller.ex
@@ -1,14 +1,37 @@
 defmodule SalvadanaioWeb.Api.V1.Analytics.BalanceController do
   use SalvadanaioWeb, :controller
+  alias Salvadanaio.Repo
+  alias Salvadanaio.Account
+  import Ecto.Query, only: [from: 2]
 
   def index(conn, _) do
-    query = "SELECT d, account, SUM(amount) FROM (
-      SELECT a.name AS account, m.amount, to_char(m.value_date, 'YYYYMM') AS d FROM movements m
-       JOIN accounts a ON a.id=m.account_id
-       WHERE value_date > CURRENT_DATE - interval '12 months') subq
-    GROUP BY d, account
-    ORDER BY account, d ASC;"
-    result = Ecto.Adapters.SQL.query!(Salvadanaio.Repo, query)
-    render conn, "index.json", balance: result.rows
+    # query = "SELECT d, account, SUM(amount) FROM (
+    #   SELECT a.name AS account, m.amount, to_char(m.value_date, 'YYYY-MM') AS d FROM movements m
+    #    JOIN accounts a ON a.id=m.account_id
+    #    WHERE value_date > CURRENT_DATE - interval '12 months') subq
+    # GROUP BY d, account
+    # ORDER BY account, d ASC;"
+    query = "SELECT SUM(amount), d, a.name, a.balance FROM (
+      SELECT amount, account_id, to_char(value_date, 'YYYY-MM') AS d
+      FROM movements
+       WHERE value_date > CURRENT_DATE - interval '12 months') m
+       join accounts a on a.id = account_id
+    GROUP BY d, a.name, a.balance
+    ORDER BY d ASC;"
+    q = Ecto.Adapters.SQL.query!(Repo, query)
+
+    result = Enum.reduce(q.rows, %{}, fn([_, _, account_id, _] = v, acc) ->
+      Map.update(acc, account_id, [v], fn(list) -> list ++ [v] end)
+    end)
+    new_result = Enum.flat_map(Map.values(result), fn(el) ->
+      [[sum, date, account_id, balance] | t] = el
+      init = [date, account_id, sum + balance]
+      then = Enum.scan(t, init, fn([sum, date, account_id, _], [_, _, old_sum] = acc) ->
+        [date, account_id, sum + old_sum]
+      end)
+      [init | then]
+    end)
+
+    render conn, "index.json", balance: new_result
   end
 end
--- backend/lib/salvadanaio_web/controllers/api/v1/analytics/movements_controller.ex
+++ backend/lib/salvadanaio_web/controllers/api/v1/analytics/movements_controller.ex
@@ -3,7 +3,7 @@ defmodule SalvadanaioWeb.Api.V1.Analytics.MovementsController do
 
   def index(conn, _) do
     query = "SELECT SUM(amount), d FROM (
-      SELECT amount, to_char(value_date, 'YYYYMM') AS d FROM movements
+      SELECT amount, to_char(value_date, 'YYYY-MM') AS d FROM movements
        WHERE value_date > CURRENT_DATE - interval '12 months') m
     GROUP BY d
     ORDER BY d ASC;"
